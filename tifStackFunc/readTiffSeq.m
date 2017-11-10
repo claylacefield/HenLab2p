dbb19397455a44585d381cf93f26df6b0f340635
claylacefield <claylacefield@gmail.com>
2017-11-10 16:56:42 -0500

    adding jessica event detection

---
+++ tifStackFunc/readTiffSeq.m
@@ -0,0 +1,31 @@
+function [Y] = readTiffSeq()
+
+
+folderpath = uigetdir();
+
+cd(folderpath);
+
+tifDir = dir;
+
+numFrames = sum(contains({tifDir.name}, '.ome.tif'));
+firstFrame = 1;
+
+tic;
+for i = 1:length(tifDir)
+    if ~isempty(strfind(tifDir(i).name, '.ome.tif'))
+        tif = imread(tifDir(i).name);
+        if firstFrame == 1
+            d1 = size(tif,1);
+            d2 = size(tif, 2);
+            firstFrame = 0;
+            Y = zeros(d1,d2,numFrames);
+            filename = tifDir(i).name;
+            basename = filename(1:strfind(filename, '_Cycle')-1);
+        end
+        Y(:,:,i)= tif;  % no, need actual frame number, not dir index
+    end
+    
+end
+
+toc;
+cfs
