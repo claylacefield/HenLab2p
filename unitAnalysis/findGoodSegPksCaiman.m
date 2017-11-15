8983acf4a5dd8fbfeb00948c7f46ceac57a4406f
claylacefield <claylacefield@gmail.com>
2017-11-15 15:03:53 -0500

    few more additions to findGoodSegPksCaiman

--- unitAnalysis/findGoodSegPksCaiman.m
+++ unitAnalysis/findGoodSegPksCaiman.m
@@ -7,24 +7,28 @@ function [goodSegPosPkStruc] = findGoodSegPksCaiman(C, treadBehStruc, numbins);
 
 %numbins = 40; % number of spatial bins over the treadmill track
 fps = 15;
+
+goodSegMinPks = 1;
+greatSegMinPks = 4;
+
 n=0; goodSeg = []; goodSegEvents={};
 m=0; greatSeg = []; greatSegEvents={};
-
-for i = 1:size(C,1)
+tic;
+for i = 1:size(C,1) % go through all units
    ca = C(i,:);
-   [pks] = clayCaTransients(ca, fps);
+   [pks] = clayCaTransients(ca, fps); % detect spiking events
    
-   if length(pks) >= 1  % if this unit has at least one spike
+   if length(pks) >= goodSegMinPks  % if this unit has at least one spike
        n = n+1;
        goodSeg(n) = i;  % add to goodSeg list
        goodSegEvents{n}=pks;    % and the spike frNum
        
-       ca = zeros(length(ca),1);
+       ca = zeros(length(ca),1); % create vector of spikes from spk times
        ca(pks) = 1;
        [caPosVelStruc] = caVsPosVel(treadBehStruc, ca, numbins, 2);
        goodSegPosPks(n,:) = caPosVelStruc.binYcaSum; % position binned spikes
        
-       if length(pks) >= 4
+       if length(pks) >= greatSegMinPks
            m = m+1;
            greatSeg(m) = i;
            greatSegEvents{m}=pks;
@@ -33,7 +37,9 @@ for i = 1:size(C,1)
        
    end
 end
+toc;
 
+% save variables to output struc
 goodSegPosPkStruc.goodSeg = goodSeg;
 goodSegPosPkStruc.goodSegEvents = goodSegEvents;
 goodSegPosPkStruc.goodSegPosPks = goodSegPosPks;
@@ -41,3 +47,8 @@ goodSegPosPkStruc.greatSeg = greatSeg;
 goodSegPosPkStruc.greatSegEvents = greatSegEvents;
 goodSegPosPkStruc.greatSegPosPks = greatSegPosPks;
 
+% and just save some params
+goodSegPosPkStruc.params.numbins = numbins;
+goodSegPosPkStruc.params.fps = fps;
+goodSegPosPkStruc.params.goodSegMinPks = goodSegMinPks;
+goodSegPosPkStruc.params.greatSegMinPks = greatSegMinPks;
