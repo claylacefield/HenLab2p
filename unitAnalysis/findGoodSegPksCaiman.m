eebe4e420cf8eab151e115a1d9610cb5183bb80f
claylacefield <claylacefield@gmail.com>
2017-11-15 14:32:59 -0500

    changing findGoodSegPksCaiman to output struc

--- unitAnalysis/findGoodSegPksCaiman.m
+++ unitAnalysis/findGoodSegPksCaiman.m
@@ -1,4 +1,4 @@
-function [goodSeg, goodSegEvents, goodSegPosPks] = findGoodSegPksCaiman(C, treadBehStruc);
+function [goodSegPosPkStruc] = findGoodSegPksCaiman(C, treadBehStruc);
 
 % This script detects transients for all units, selects goodSegs as units
 % thathave more than a certain number of events, and calculates their
@@ -8,21 +8,36 @@ function [goodSeg, goodSegEvents, goodSegPosPks] = findGoodSegPksCaiman(C, tread
 numbins = 40; % number of spatial bins over the treadmill track
 fps = 15;
 n=0; goodSeg = []; goodSegEvents={};
+m=0; greatSeg = []; greatSegEvents={};
 
 for i = 1:size(C,1)
    ca = C(i,:);
    [pks] = clayCaTransients(ca, fps);
-   if length(pks) > 5
+   
+   if length(pks) >= 1  % if this unit has at least one spike
        n = n+1;
-       goodSeg(n) = i;
-       goodSegEvents{n}=pks;
+       goodSeg(n) = i;  % add to goodSeg list
+       goodSegEvents{n}=pks;    % and the spike frNum
        
        ca = zeros(length(ca),1);
        ca(pks) = 1;
        [caPosVelStruc] = caVsPosVel(treadBehStruc, ca, numbins, 2);
-       goodSegPosPks(n,:) = caPosVelStruc.binYcaSum;
+       goodSegPosPks(n,:) = caPosVelStruc.binYcaSum; % position binned spikes
+       
+       if length(pks) >= 4
+           m = m+1;
+           greatSeg(m) = i;
+           greatSegEvents{m}=pks;
+           greatSegPosPks(m,:) = caPosVelStruc.binYcaSum;
+       end
+       
    end
 end
 
-
+goodSegPosPkStruc.goodSeg = goodSeg;
+goodSegPosPkStruc.goodSegEvents = goodSegEvents;
+goodSegPosPkStruc.goodSegPosPks = goodSegPosPks;
+goodSegPosPkStruc.greatSeg = greatSeg;
+goodSegPosPkStruc.greatSegEvents = greatSegEvents;
+goodSegPosPkStruc.greatSegPosPks = greatSegPosPks;
 
