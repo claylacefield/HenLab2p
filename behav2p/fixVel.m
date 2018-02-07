7457d1726e9fcfb68cde85b82b009abaa43f56dd
claylacefield <claylacefield@gmail.com>
2018-02-07 13:49:46 -0500

    made fixVel.m and changed plotCNMF_AC to auto transpose AC input

---
+++ behav2p/fixVel.m
@@ -0,0 +1,19 @@
+function [vel] = fixVel(vel);
+
+%% USAGE: [vel] = fixVel(vel);
+
+% This function fixes problems with the velocity measurements at the belt
+% transition (just makes equal to previous good value)
+
+
+for i = 1:length(vel)
+    try
+        if vel(i)>nanstd(vel) %|| isnan(vel(i))
+            vel(i) = lastGoodVel;
+        else
+            lastGoodVel = vel(i);
+        end
+    catch
+        vel(i) = 0;
+    end
+end
