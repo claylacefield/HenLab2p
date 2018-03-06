98c36958303e8a0c2845af33cb3133440925c43a
clay <claylacefield@gmail.com>
2018-03-06 10:41:23 -0500

    few minor changes

--- unitAnalysis/clayMRL.m
+++ unitAnalysis/clayMRL.m
@@ -7,11 +7,11 @@ radBinSiz = 2*pi/numBins;
 
 xSum = 0; ySum = 0;
 for i = 1:numBins
-    xSum = xSum + binCaAvg(i)*cos((i-1)*radBinSiz);
-    ySum = ySum + binCaAvg(i)*sin((i-1)*radBinSiz);
+    xSum = xSum + binCaAvg(i)*cos((i-1)*radBinSiz); % x = r*cos(theta);
+    ySum = ySum + binCaAvg(i)*sin((i-1)*radBinSiz); % y = r*sin(theta);
 end
 
-xAv = xSum/numBins;
+xAv = xSum/numBins; % actually don't need avg over bins
 yAv = ySum/numBins;
 
 theta = atan2(ySum, xSum);
@@ -19,7 +19,8 @@ theta = atan2(ySum, xSum);
 thetaPi = theta/pi;  % theta in pi radians
 
 meanAngRad = theta;
-meanLength = sqrt(xAv^2 + yAv^2);
+%meanLength = sqrt(xAv^2 + yAv^2);
+meanLength = sqrt(xSum^2 + ySum^2);
 
 rho = meanLength;
 
