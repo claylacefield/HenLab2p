2bc182dbfc1ac3338353e5681dffc84575712504
clay <claylacefield@gmail.com>
2018-03-11 00:23:00 -0500

    added vel and pos buttons to guiSelectDGCs

--- guis/guiSelectDGCs.m
+++ guis/guiSelectDGCs.m
@@ -1,35 +1,35 @@
-function varargout = guiSelectDGCs_180308a(varargin)
-% GUISELECTDGCS_180308A MATLAB code for guiSelectDGCs_180308a.fig
-%      GUISELECTDGCS_180308A, by itself, creates a new GUISELECTDGCS_180308A or raises the existing
+function varargout = guiSelectDGCs(varargin)
+% GUISELECTDGCS MATLAB code for guiSelectDGCs.fig
+%      GUISELECTDGCS, by itself, creates a new GUISELECTDGCS or raises the existing
 %      singleton*.
 %
-%      H = GUISELECTDGCS_180308A returns the handle to a new GUISELECTDGCS_180308A or the handle to
+%      H = GUISELECTDGCS returns the handle to a new GUISELECTDGCS or the handle to
 %      the existing singleton*.
 %
-%      GUISELECTDGCS_180308A('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in GUISELECTDGCS_180308A.M with the given input arguments.
+%      GUISELECTDGCS('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUISELECTDGCS.M with the given input arguments.
 %
-%      GUISELECTDGCS_180308A('Property','Value',...) creates a new GUISELECTDGCS_180308A or raises the
+%      GUISELECTDGCS('Property','Value',...) creates a new GUISELECTDGCS or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before guiSelectDGCs_180308a_OpeningFcn gets called.  An
+%      applied to the GUI before guiSelectDGCs_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to guiSelectDGCs_180308a_OpeningFcn via varargin.
+%      stop.  All inputs are passed to guiSelectDGCs_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help guiSelectDGCs_180308a
+% Edit the above text to modify the response to help guiSelectDGCs
 
-% Last Modified by GUIDE v2.5 10-Mar-2018 00:21:34
+% Last Modified by GUIDE v2.5 11-Mar-2018 00:11:17
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @guiSelectDGCs_180308a_OpeningFcn, ...
-                   'gui_OutputFcn',  @guiSelectDGCs_180308a_OutputFcn, ...
+                   'gui_OpeningFcn', @guiSelectDGCs_OpeningFcn, ...
+                   'gui_OutputFcn',  @guiSelectDGCs_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,28 +44,30 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before guiSelectDGCs_180308a is made visible.
-function guiSelectDGCs_180308a_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before guiSelectDGCs is made visible.
+function guiSelectDGCs_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to guiSelectDGCs_180308a (see VARARGIN)
+% varargin   command line arguments to guiSelectDGCs (see VARARGIN)
 
-% Choose default command line output for guiSelectDGCs_180308a
+% Choose default command line output for guiSelectDGCs
 handles.output = hObject;
 
 handles.goodSeg = [];
+handles.plotPos = 0;
+handles.plotVel = 0;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes guiSelectDGCs_180308a wait for user response (see UIRESUME)
+% UIWAIT makes guiSelectDGCs wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = guiSelectDGCs_180308a_OutputFcn(hObject, eventdata, handles) 
+function varargout = guiSelectDGCs_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -73,36 +75,6 @@ function varargout = guiSelectDGCs_180308a_OutputFcn(hObject, eventdata, handles
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
-
-
-% --- Executes on slider movement.
-function segNumSlider_Callback(hObject, eventdata, handles)
-% hObject    handle to segNumSlider (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-
-segNum = int32(get(handles.segNumSlider, 'Value'));
-set(handles.segTxt, 'String', num2str(segNum));
-handles.segNum = segNum;
-
-A = handles.A; C = handles.C;
-aSeg = reshape(full(A(:,segNum)),handles.d1,handles.d2);
-imagesc(handles.spatialAxes, aSeg);
-plot(handles.temporalAxes, C(segNum,:));
-
-try
-sAll = handles.sAll; out = handles.out;
-plot(handles.posTuneAxes, out.Shuff.ThreshRate(segNum,:));
-hold(handles.posTuneAxes, 'on');
-
-plot(handles.posTuneAxes, out.posRates(segNum,:));
-hold(handles.posTuneAxes, 'off');
-catch
-end
-
-guidata(hObject, handles);
 
 % --- Executes on button press in loadDataButton.
 function loadDataButton_Callback(hObject, eventdata, handles)
@@ -121,17 +93,62 @@ handles.A = A;
 handles.d1 = d1;
 handles.d2 = d2;
 handles.treadBehStruc = treadBehStruc;
-
-handles.binVel = binByLocation(fixVel(treadBehStruc.vel(1:2:end)), pos, 100);
+vel = fixVel(treadBehStruc.vel(1:2:end));
+handles.vel = vel;
+handles.treadPos = treadPos; 
+%handles.
 
 try
 handles.sAll = sAll;
 handles.out = out;
+
+handles.binVel = binByLocation(vel, treadPos, 100);
 catch
     disp('No tuning info');
 end
 
 guidata(hObject, handles);
+
+% --- Executes on slider movement.
+function segNumSlider_Callback(hObject, eventdata, handles)
+% hObject    handle to segNumSlider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+segNum = int32(get(handles.segNumSlider, 'Value'));
+set(handles.segTxt, 'String', num2str(segNum));
+handles.segNum = segNum;
+
+A = handles.A; C = handles.C;
+aSeg = reshape(full(A(:,segNum)),handles.d1,handles.d2);
+imagesc(handles.spatialAxes, aSeg);
+
+if handles.plotPos == 1
+plot(handles.temporalAxes, handles.treadPos/50, 'c');
+hold(handles.temporalAxes, 'on');
+end
+if handles.plotVel == 1
+plot(handles.temporalAxes, handles.vel/500, 'g');
+hold(handles.temporalAxes, 'on');
+end
+plot(handles.temporalAxes, C(segNum,:));
+hold(handles.temporalAxes, 'off');
+
+try
+sAll = handles.sAll; out = handles.out;
+plot(handles.posTuneAxes, out.Shuff.ThreshRate(segNum,:));
+hold(handles.posTuneAxes, 'on');
+plot(handles.posTuneAxes, handles.binVel/2000, 'g.');
+plot(handles.posTuneAxes, out.posRates(segNum,:));
+hold(handles.posTuneAxes, 'off');
+catch
+end
+
+guidata(hObject, handles);
+
+
 
 % --- Executes during object creation, after setting all properties.
 function segNumSlider_CreateFcn(hObject, eventdata, handles)
@@ -238,9 +255,57 @@ end
 
 [b,inds]=sort(maxInd);
 posRates = posRates(inds,:);
+plot(handles.popAvgAxes, handles.binVel/20, 'g.');
+hold(handles.popAvgAxes, 'on');
 plot(handles.popAvgAxes, mean(posRates,1));
+hold(handles.popAvgAxes, 'off');
 imagesc(handles.goodPosTuneAxes, posRates);
 catch
 end
+
+guidata(hObject, handles);
+
+
+% --- Executes on button press in plotVelCheckbox.
+function plotVelCheckbox_Callback(hObject, eventdata, handles)
+% hObject    handle to plotVelCheckbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of plotVelCheckbox
+handles.plotVel = int32(get(handles.plotVelCheckbox, 'Value'));
+
+if handles.plotPos == 1
+plot(handles.temporalAxes, handles.treadPos/50, 'c');
+hold(handles.temporalAxes, 'on');
+end
+if handles.plotVel == 1
+plot(handles.temporalAxes, handles.vel/500, 'g');
+hold(handles.temporalAxes, 'on');
+end
+plot(handles.temporalAxes, handles.C(handles.segNum,:));
+hold(handles.temporalAxes, 'off');
+
+guidata(hObject, handles);
+
+% --- Executes on button press in plotPosCheckbox.
+function plotPosCheckbox_Callback(hObject, eventdata, handles)
+% hObject    handle to plotPosCheckbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of plotPosCheckbox
+handles.plotPos = int32(get(handles.plotPosCheckbox, 'Value'));
+
+if handles.plotPos == 1
+plot(handles.temporalAxes, handles.treadPos/50, 'c');
+hold(handles.temporalAxes, 'on');
+end
+if handles.plotVel == 1
+plot(handles.temporalAxes, handles.vel/500, 'g');
+hold(handles.temporalAxes, 'on');
+end
+plot(handles.temporalAxes, handles.C(handles.segNum,:));
+hold(handles.temporalAxes, 'off');
 
 guidata(hObject, handles);
