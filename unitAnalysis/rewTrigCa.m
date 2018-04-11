c3d82979c256f7c82a7698e1711d9461661c6043
clay <claylacefield@gmail.com>
2018-04-11 02:06:48 -0400

    wrote rewTrigCa.m for unit rew sig

--- unitAnalysis/rewTrigCa.m
+++ unitAnalysis/rewTrigCa.m
@@ -2,6 +2,8 @@ function [periEvent] = rewTrigCa(ca, treadBehStruc); %, event)
 
 rewTime = treadBehStruc.rewTime;
 frTimes = treadBehStruc.adjFrTimes(1:2:end);
+vel = treadBehStruc.vel(1:2:end);
+vel = fixVel(vel);
 
 rewStartTimes = rewTime([1 find(diff(rewTime)>10)+1]);
 
@@ -11,21 +13,28 @@ for i = 1:length(frInds)
     for j = 1:size(ca,1)
     try
     periEvent(j,:,i) = ca(j,frInds(i)-5*fps:frInds(i)+10*fps);
+    
     catch
     end
     end
+    try
+    periEventVel(:,i) = vel(frInds(i)-5*fps:frInds(i)+10*fps);
+    catch
+    end
 end
+
 
 squeeze(periEvent);
 
-pe = mean(periEvent,3)';
+pe = nanmean(periEvent,3)';
 figure('Position', [0 0 1000 400]); 
 subplot(1,2,1);
-plotMeanSEMshaderr(pe,'b'); hold on; line([76 76], [0.5e4 2e4]);
+plotMeanSEMshaderr(pe,'b'); 
+%hold on; line([76 76], [0.5e4 2e4]);
 xlim([0 226]);
-
 subplot(1,2,2);
-plot(x,pe);hold on; line([0 0], [-2e4 16e4]);
+plot(x,pe);
+%hold on; line([0 0], [-2e4 16e4]);
 
 
 
