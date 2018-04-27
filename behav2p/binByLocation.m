d7067ac7b34fbe437e1e5ee0a982f2113c8e5636
claylacefield <claylacefield@gmail.com>
2018-04-27 16:59:13 -0400

    few changes to new wrapTuningNewClay.m

--- behav2p/binByLocation.m
+++ behav2p/binByLocation.m
@@ -1,11 +1,13 @@
-function [binCaAvg] = binByLocation(ca, pos, numBins)
+function [binCaAvg] = binByLocation(ca, pos, numBins);
 
 % USAGE: [binCaAvg] = binByLocation(ca, pos, numBins);
 
 [counts, edges, binInd] = histcounts(pos, numBins);
 
-
-for i = 1:numBins
-    binCa = ca(binInd == i);
-    binCaAvg(i) = mean(binCa);
+for j=1:size(ca,1)
+    for i = 1:numBins
+        inds = find(binInd==i);
+        binCa = ca(j,inds);
+        binCaAvg(j,i) = mean(binCa);
+    end
 end
