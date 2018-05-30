ced8dad88cdbf3ff7b91f7ddb56a91d54023126d
clay <claylacefield@gmail.com>
2018-05-29 23:52:01 -0400

    pretty happy with findAccelDecel.m

--- behav2p/findAccelDecel.m
+++ behav2p/findAccelDecel.m
@@ -1,14 +1,32 @@
-function [accelStruc] = findAccelDecel(treadBehStruc)
+function [accelPks, decelPks] = findAccelDecel(treadBehStruc, toPlot)
 
 % Function to identify times of treadmill acceleration and deceleration
 % Clay, May 2018
 
 
 vel = treadBehStruc.vel;
-vel = fixVel(vel);
+vel = fixVel(vel); % fix velocity for lap boundaries, etc.
 
-vel = vel(1:2:end);
+vel = vel(1:2:end); % downsample
 
-v2 = runmean(vel,15);
+v2 = runmean(vel,15); % smooth velocity 
 v2 = runmean(v2,30);
+
+dv = [0 diff(v2)]; % slope of smoothed velocity
+sdv = std(dv); % standard dev
+
+[vals, accelPks] = findpeaks(dv, 'MinPeakProminence', sdv*2, 'MinPeakDistance', 100);
+
+[vals, decelPks] = findpeaks(-dv, 'MinPeakProminence', sdv*2, 'MinPeakDistance', 100);
+
+if toPlot
+t = 1:length(dv);
+figure; plot(dv); 
+hold on; 
+plot(t(accelPks), dv(accelPks), 'g*');
+plot(t(decelPks), dv(decelPks), 'r*');
+end
+
+
+
 
