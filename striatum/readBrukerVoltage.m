55f192a40e386685d52d97c0fde5f787eebd25a2
claylacefield <claylacefield@gmail.com>
2018-06-20 17:55:47 -0400

    added new datAxonFrameSigPks.m and readBrukerVoltage.m scripts

---
+++ striatum/readBrukerVoltage.m
@@ -0,0 +1,13 @@
+function [v, vTimes, ttlOn, ttlOff] = readBrukerVoltage()
+
+[filename, path] = uigetfile('*.csv', 'Select Bruker .csv exported voltage recording');
+cd(path);
+
+inStruc = importdata(filename);
+
+v = inStruc.data(:,2);
+vTimes = inStruc.data(:,1);
+
+[vals, ttlOn] = findpeaks(diff(v), vTimes, 'MinPeakProminence',3, 'MinPeakDistance', 500);
+
+[vals, ttlOff] = findpeaks(-diff(v), vTimes, 'MinPeakProminence',3, 'MinPeakDistance', 500);
