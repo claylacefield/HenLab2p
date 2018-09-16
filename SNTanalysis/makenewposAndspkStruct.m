52e234be8f756d1ecd5433ad20edc76d25e27420
claylacefield <claylacefield@gmail.com>
2018-09-16 18:11:57 -0400

    ST-add new goodSegs

--- SNTanalysis/makenewposAndspkStruct.m
+++ SNTanalysis/makenewposAndspkStruct.m
@@ -1,4 +1,4 @@
-%%to get only goodsepks,leave others blank so it keeps correct seg index
+%%to get only goodsegpks,leave others blank so it keeps correct seg index
 %%from C
 allIndexes = 1: length (pksCell);
 goodInd = ismember(allIndexes, goodSeg);
@@ -7,3 +7,10 @@ pksgoodSeg = pksCell;
 for i = 1:length(badInd)
     pksgoodSeg{badInd(i)}= [];
 end
+
+[treadBehStruc] = procHen2pBehav();
+BigposAndSpkArray {1,3}.C=C;
+BigposAndSpkArray {1,3}.pksgoodSeg=pksgoodSeg;
+BigposAndSpkArray {1,3}.treadBehStruct=treadBehStruc;
+BigposAndSpkArray {1,3}.filename='XIR.0718.02_500-003';
+
--- unitAnalysis/findGoodSegPksCaiman_AG.m
+++ unitAnalysis/findGoodSegPksCaiman_AG.m
@@ -1,12 +1,12 @@
-function [goodSeg, goodSegEvents, goodSegPosPks, pksAll] = findGoodSegPksCaiman(C, treadBehStruc);
+function [goodSegAll, goodSegEvents, goodSegPosPks, pksAll] = findGoodSegPksCaiman_AG(C, treadBehStruc);
 
 % This script detects transients for all units, selects goodSegs as units
 % thathave more than a certain number of events, and calculates their
-% spatial tuning.
+% spatial tuning. Does not consider gui selected goodSegs (ST)
 % Clay 2017
 
 fps = 15;
-n=0; goodSeg = []; goodSegEvents={}; 
+n=0; goodSegAll = []; goodSegEvents={}; 
 pksAll = {};
 for i = 1:size(C,1)
    ca = C(i,:);
@@ -14,7 +14,7 @@ for i = 1:size(C,1)
    pksAll{i} = pks;
    if length(pks) <4
        n = n+1;
-       goodSeg(n) = i;
+       goodSegAll(n) = i;
        goodSegEvents{n}=pks;
        
        ca = zeros(length(ca),1);
