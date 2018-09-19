409bad6efbe02d992035235981395bee23d15609
claylacefield <claylacefield@gmail.com>
2018-09-19 13:24:44 -0400

    committing any recent changes

---
+++ unitAnalysis/multSession/sameCellRemapSubsets.m
@@ -0,0 +1,202 @@
+function [remapStrucSubsets] = sameCellRemapSubsets(sameCellTuningStruc);
+
+%% USAGE: [remapStruc] = sameCellRemap(sameCellTuningStruc);
+
+% Clay Dec. 2017, modified Sebnem Sept 2018
+% find cells active in subsets of sessions (or are place cells in
+% subsets of sessions?)
+% May2018
+% Now including more followup analyses
+% remapStruc.sessComb = ziv rows with cells present in diff combin of sess
+% remapStruc.allZivCorrCoef = cell array of corr coeff for all ziv cells
+% 
+
+
+%% unpack some variables
+multSessSegStruc = sameCellTuningStruc.multSessSegStruc; % just save orig struc (not too huge)
+unitSpatCell = sameCellTuningStruc.unitSpatCell;  % cell array of spatial profiles of ziv cells
+zivCentroids = sameCellTuningStruc.zivCentroids;    % centroids of these cells
+placeCellOrigInd = sameCellTuningStruc.placeCellOrigInd;  % ind of place cells (Andres) w. re. to orig C/A
+rewCellOrigInd = sameCellTuningStruc.rewCellOrigInd;
+cellsInAll = sameCellTuningStruc.cellsInAll; % orig C/A index of all ziv registered cells present in all sessions
+placeCellAllOrigInd = sameCellTuningStruc.placeCellAllOrigInd;% orig C/A index of all cells that are place cells in all sessions
+placeCellAnyOrigInd = sameCellTuningStruc.placeCellInAnyOrigInd;
+placeCellNoneOrigInd = sameCellTuningStruc.placeCellInNoneOrigInd;
+rewCellAllOrigInd = sameCellTuningStruc.rewCellAllOrigInd; 
+rewCellNoneOrigInd = sameCellTuningStruc.rewCellInNoneOrigInd; 
+rewCellAnyOrigInd = sameCellTuningStruc.rewCellInAnyOrigInd; 
+regMapOrigInd = sameCellTuningStruc.regMapOrigInd; % orig C/A ind for all cells in ziv mat
+regMapGoodSegInd = sameCellTuningStruc.regMapGoodSegInd;
+sameCellPlaceBool = sameCellTuningStruc.sameCellPlaceBool;  % boolean for this mat (no its not)
+
+
+%% find place cells in diff combin of sessions
+% NOTE: indices reference ziv array, e.g. regMapOrigInd
+n=0;m=0;p=0;q=0;r=0;s=0;
+zivMatBool = regMapOrigInd;
+zivMatBool(zivMatBool~=0)=1;
+for i = 1:size(zivMatBool,1)
+    pcs = zivMatBool(i,:);
+    if pcs==[1,0,0]
+        n=n+1;
+        sessComb.firstOnly(n) = i;
+    elseif pcs==[1,1,0]
+        r=r+1;
+        sessComb.firstTwoOnly(r) = i;
+    elseif pcs==[0,1,1]
+        m=m+1;
+        sessComb.lastTwoOnly(m) = i;
+    elseif pcs==[0,1,0]
+        p=p+1;
+        sessComb.secondOnly(p) = i;
+    elseif pcs==[0,0,1]
+        q=q+1;
+        sessComb.lastOnly(q) = i;
+        elseif pcs==[1,1,1]
+        s=s+1;
+        sessComb.allSess(s) = i;
+    end
+end
+remapStrucSubsets.sessComb = sessComb;
+
+figure;
+bar([length(sessComb.firstTwoOnly) length(sessComb.lastTwoOnly)]);
+title('#Reg cells in 1-2, 2-3 (may be affected by reg)');
+
+%% Fractions of cells
+% note that tuning in PCLapSess struc only performed for goodSeg
+
+for i = 1:length(multSessSegStruc)
+    pcInds = find(multSessSegStruc(i).PCLapSess.Shuff.isPC);
+    remapStrucSubsets.numPCs(i) = length(pcInds);
+    remapStrucSubsets.fracPCs(i) = remapStrucSubsets.numPCs(i)/length(multSessSegStruc(i).goodSeg);
+end
+
+figure; 
+subplot(1,2,1);
+bar(remapStrucSubsets.numPCs);
+title('numPCs');
+subplot(1,2,2);
+bar(remapStrucSubsets.fracPCs);
+title('fracPCs');
+
+%% for cells with reward related activity in nonmove periods in at least one session (rewCellAny)
+
+% find goodSeg indices (for extracting tuning from )
+for i = 1:size(rewCellAnyOrigInd,1)
+    for j = 1:length(multSessSegStruc)
+        rewCellAnyGoodSegInd(i,j) = find(multSessSegStruc(j).goodSeg == rewCellAnyOrigInd(i,j));
+        posRatesCellAnyRew{i,j} = multSessSegStruc(j).outNonPC.posRates(rewCellAnyGoodSegInd(i,j),:);
+    end
+end
+
+% correlate tuning for AnyPC
+for i = 1:size(posRatesCellAnyRew,1)
+    rates = [];
+    for j = 1:size(posRatesCellAnyRew,2)
+        rates = [rates; posRatesCellAnyRew{i,j}];
+    end
+    [remapStrucSubsets.rewInAnyCoef{i}, remapStrucSubsets.rewInAnyPval{i}] = corrcoef(rates');
+end
+
+A1A2=[]; A1B=[]; A2B=[];
+for i = 1:length(remapStrucSubsets.rewInAnyCoef)
+    A1A2 = [A1A2; remapStrucSubsets.rewInAnyCoef{i}(2)];
+    A1B = [A1B; remapStrucSubsets.rewInAnyCoef{i}(3)];
+    A2B = [A2B; remapStrucSubsets.rewInAnyCoef{i}(6)];
+end
+remapStrucSubsets.RewAnyCorrCoeff121323 = [A1A2, A1B, A2B];
+
+%plot tunig sorted by sess2
+RewMatchedAny = {};% make a cell array of all posRates together
+for i = 1:size(posRatesCellAnyRew, 2)
+    RewMatchedAny{i} = [];
+    for ii = 1:size(posRatesCellAnyRew, 1)
+        RewMatchedAny{i} = [RewMatchedAny{i}; posRatesCellAnyRew{ii, i}];
+    end
+end
+remapStrucSubsets.RewMatchedAny = [RewMatchedAny];
+
+%sort second column and match 1 and 3 accordingly: 
+[~, s1] = nanmax(RewMatchedAny{2}, [], 2);
+[~, s2] = sort(s1);
+for i = 1:length(RewMatchedAny)
+    RewMatchedAny{i} = RewMatchedAny{i}(s2, :);
+end
+figure;
+for i = 1:3
+    subplot(1, 3, i);
+    imagesc(RewMatchedAny{i});
+end
+
+%normalize each by max firing rate
+for i = 1:length(RewMatchedAny)
+    for ii = 1:size(RewMatchedAny{i}, 1)
+        RewMatchedAny{i}(ii, :) = RewMatchedAny{i}(ii, :)/nanmax(RewMatchedAny{i}(ii, :));
+    end
+    RewMatchedAny{i}(isnan(RewMatchedAny{i})) = 0;
+end
+
+figure;
+for i = 1:3
+    subplot(1, 3, i);
+    imagesc(RewMatchedAny{i});
+end
+
+
+
+%% find posRates for all cells in mapInd2/regMapOrigInd (ziv regist matr) 
+for i = 1:size(regMapOrigInd,1)
+    for j = 1:length(multSessSegStruc)
+        try
+            goodSegInd(i,j) = find(multSessSegStruc(j).goodSeg == regMapOrigInd(i,j));
+            posRatesCell{i,j} = multSessSegStruc(j).outPC.posRates(goodSegInd(i,j),:);
+        catch
+            posRatesCell{i,j} = [];
+        end
+    end
+end
+
+% correlate tuning for all cells in ziv matrix
+for i = 1:size(posRatesCell,1)
+    rates = [];
+    for j = 1:size(posRatesCell,2)
+        rates = [rates; posRatesCell{i,j}];
+    end
+    [remapStrucSubsets.allZivCorrCoef{i}, remapStrucSubsets.allZivCorrPval{i}] = corrcoef(rates');
+end
+
+
+%% now use this to compare correlations bet. 1-2, 2-3
+% 
+firstTwoPcell = remapStrucSubsets.allZivCorrPval(remapStrucSubsets.sessComb.firstTwoOnly);
+firstTwoCorrCell = remapStrucSubsets.allZivCorrCoef(remapStrucSubsets.sessComb.firstTwoOnly);
+for i = 1:length(firstTwoPcell)
+    firstTwoP(i)=firstTwoPcell{i}(2);
+    firstTwoCorr(i)=firstTwoCorrCell{i}(2);
+end
+
+lastTwoPcell = remapStrucSubsets.allZivCorrPval(remapStrucSubsets.sessComb.lastTwoOnly);
+lastTwoCorrCell = remapStrucSubsets.allZivCorrCoef(remapStrucSubsets.sessComb.lastTwoOnly);
+for i = 1:length(lastTwoPcell)
+    lastTwoP(i)=lastTwoPcell{i}(2);
+    lastTwoCorr(i)=lastTwoCorrCell{i}(2);
+end
+
+
+figure('Position', [50 50 800 400]); 
+subplot(1,2,1);
+f = firstTwoCorr(firstTwoP<0.05);
+l = lastTwoCorr(lastTwoP<0.05);
+bar([nanmean(f) nanmean(l)]); hold on;
+sem12 = nanstd(f)/sqrt(length(f));
+sem23 = nanstd(l)/sqrt(length(l));
+errorbar([nanmean(f) nanmean(l)],[sem12 sem23], '.');
+title('mean tuning correl, sig pval, sess 1-2 vs. 2-3');
+
+subplot(1,2,2);
+bar([nanmean(firstTwoCorr) nanmean(lastTwoCorr)]); hold on;
+sem12b = nanstd(firstTwoCorr)/sqrt(length(firstTwoCorr));
+sem23b = nanstd(lastTwoCorr)/sqrt(length(lastTwoCorr));
+errorbar([nanmean(firstTwoCorr) nanmean(lastTwoCorr)],[sem12b sem23b], '.');
+title('mean tuning correl, sess 1-2 vs. 2-3');
---
+++ unitAnalysis/sameCellRemap.m
@@ -0,0 +1,287 @@
+function [remapStruc] = sameCellRemap(sameCellTuningStruc);
+
+%% USAGE: [remapStruc] = sameCellRemap(sameCellTuningStruc);
+
+% Clay Dec. 2017
+% find cells active in subsets of sessions (or are place cells in
+% subsets of sessions?)
+% May2018
+% Now including more followup analyses
+% remapStruc.sessComb = ziv rows with cells present in diff combin of sess
+% remapStruc.allZivCorrCoef = cell array of corr coeff for all ziv cells
+% 
+
+
+%% unpack some variables
+multSessSegStruc = sameCellTuningStruc.multSessSegStruc; % just save orig struc (not too huge)
+unitSpatCell = sameCellTuningStruc.unitSpatCell;  % cell array of spatial profiles of ziv cells
+zivCentroids = sameCellTuningStruc.zivCentroids;    % centroids of these cells
+placeCellOrigInd = sameCellTuningStruc.placeCellOrigInd;  % ind of place cells (Andres) w. re. to orig C/A
+cellsInAll = sameCellTuningStruc.cellsInAll; % orig C/A index of all ziv registered cells present in all sessions
+placeCellAllOrigInd = sameCellTuningStruc.placeCellAllOrigInd; % orig C/A index of all cells that are place cells in all sessions
+placeCellInNoneOrigInd = sameCellTuningStruc.placeCellInNoneOrigInd ;
+placeCellInAnyOrigInd = sameCellTuningStruc.placeCellInAnyOrigInd;
+regMapOrigInd = sameCellTuningStruc.regMapOrigInd; % orig C/A ind for all cells in ziv mat
+regMapGoodSegInd = sameCellTuningStruc.regMapGoodSegInd;
+sameCellPlaceBool = sameCellTuningStruc.sameCellPlaceBool;  % boolean for this mat (no its not)
+
+
+%% find place cells in diff combin of sessions
+% NOTE: indices reference ziv array, e.g. regMapOrigInd
+n=0;m=0;p=0;q=0;r=0;s=0;
+zivMatBool = regMapOrigInd;
+zivMatBool(zivMatBool~=0)=1;
+for i = 1:size(zivMatBool,1)
+    pcs = zivMatBool(i,:);
+    if pcs==[1,0,0]
+        n=n+1;
+        sessComb.firstOnly(n) = i;
+    elseif pcs==[1,1,0]
+        r=r+1;
+        sessComb.firstTwoOnly(r) = i;
+    elseif pcs==[0,1,1]
+        m=m+1;
+        sessComb.lastTwoOnly(m) = i;
+    elseif pcs==[0,1,0]
+        p=p+1;
+        sessComb.secondOnly(p) = i;
+    elseif pcs==[0,0,1]
+        q=q+1;
+        sessComb.lastOnly(q) = i;
+        elseif pcs==[1,1,1]
+        s=s+1;
+        sessComb.allSess(s) = i;
+    end
+end
+remapStruc.sessComb = sessComb;
+
+figure;
+bar([length(sessComb.firstTwoOnly) length(sessComb.lastTwoOnly)]);
+title('#Reg cells in 1-2, 2-3 (may be affected by reg)');
+
+%% Fractions of cells
+% note that tuning in outPC struc only performed for goodSeg
+
+for i = 1:length(multSessSegStruc)
+    pcInds = find(multSessSegStruc(i).PCLapSess.Shuff.isPC);
+    remapStruc.numPCs(i) = length(pcInds);
+    remapStruc.fracPCs(i) = remapStruc.numPCs(i)/length(multSessSegStruc(i).goodSeg);
+end
+
+figure; 
+subplot(1,2,1);
+bar(remapStruc.numPCs);
+title('numPCs');
+subplot(1,2,2);
+bar(remapStruc.fracPCs);
+title('fracPCs');
+
+
+%% tuning xcorr for cells that are place cells in all three sessions
+
+% find goodSeg indices (for extracting tuning from PCLapSess)
+for i = 1:size(placeCellAllOrigInd,1)
+    for j = 1:length(multSessSegStruc)
+        placeCellAllGoodSegInd(i,j) = find(multSessSegStruc(j).goodSeg == placeCellAllOrigInd(i,j));
+        posRatesCell{i,j} = multSessSegStruc(j).PCLapSess.posRates(placeCellAllGoodSegInd(i,j),:);
+    end
+end
+
+% correlate tuning for cells that are place cells in all sessions
+for i = 1:size(posRatesCell,1)
+    rates = [];
+    for j = 1:size(posRatesCell,2)
+        rates = [rates; posRatesCell{i,j}];
+    end
+    [remapStruc.pcInAllCoef{i}, remapStruc.pcInAllPval{i}] = corrcoef(rates');
+end
+
+A1A2=[]; A1B=[]; A2B=[];
+for i = 1:length(remapStruc.pcInAllCoef)
+    A1A2 = [A1A2; remapStruc.pcInAllCoef{i}(2)];
+    A1B = [A1B; remapStruc.pcInAllCoef{i}(3)];
+    A2B = [A2B; remapStruc.pcInAllCoef{i}(6)];
+end
+remapStruc.PCCorrCoeff121323 = [A1A2, A1B, A2B];
+
+%plot tunig sorted by sess2
+PCMatchedAll = {};% make a cell array of all posRates together
+for i = 1:size(posRatesCellAllPC, 2)
+    PCMatchedAll{i} = [];
+    for ii = 1:size(posRatesCellAllPC, 1)
+        PCMatchedAll{i} = [PCMatchedAll{i}; posRatesCellAllPC{ii, i}];
+    end
+end
+remapStruc.PCMatchedAll = [PCMatchedAll];
+
+%sort second column and match 1 and 3 accordingly: 
+[~, s1] = nanmax(PCMatchedAll{2}, [], 2);
+[~, s2] = sort(s1);
+for i = 1:length(PCMatchedAll)
+    PCMatchedAll{i} = PCMatchedAll{i}(s2, :);
+end
+figure;
+for i = 1:3
+    subplot(1, 3, i);
+    imagesc(PCMatchedAll{i});
+end
+
+%normalize each by max firing rate
+for i = 1:length(PCMatchedAll)
+    for ii = 1:size(PCMatchedAll{i}, 1)
+        PCMatchedAll{i}(ii, :) = PCMatchedAll{i}(ii, :)/nanmax(PCMatchedAll{i}(ii, :));
+    end
+    PCMatchedAll{i}(isnan(PCMatchedAll{i})) = 0;
+end
+
+figure;
+for i = 1:3
+    subplot(1, 3, i);
+    imagesc(PCMatchedAll{i});
+end
+
+%% tuningxcorr for cells that are place cells in at least one session (PCAny)
+
+% find goodSeg indices (for extracting tuning from outPC)
+for i = 1:size(placeCellAnyOrigInd,1)
+    for j = 1:length(multSessSegStruc)
+        placeCellAnyGoodSegInd(i,j) = find(multSessSegStruc(j).goodSeg == placeCellAnyOrigInd(i,j));
+        posRatesCellAnyPC{i,j} = multSessSegStruc(j).PCLapSess.posRates(placeCellAnyGoodSegInd(i,j),:);
+    end
+end
+
+% correlate tuning for AnyPC
+for i = 1:size(posRatesCellAnyPC,1)
+    rates = [];
+    for j = 1:size(posRatesCellAnyPC,2)
+        rates = [rates; posRatesCellAnyPC{i,j}];
+    end
+    [remapStrucSubsets.pcInAnyCoef{i}, remapStrucSubsets.pcInAnyPval{i}] = corrcoef(rates');
+end
+
+A1A2=[]; A1B=[]; A2B=[];
+for i = 1:length(remapStrucSubsets.pcInAnyCoef)
+    A1A2 = [A1A2; remapStrucSubsets.pcInAnyCoef{i}(2)];
+    A1B = [A1B; remapStrucSubsets.pcInAnyCoef{i}(3)];
+    A2B = [A2B; remapStrucSubsets.pcInAnyCoef{i}(6)];
+end
+remapStrucSubsets.PCAnyCorrCoeff121323 = [A1A2, A1B, A2B];
+
+%plot tunig sorted by sess2
+PCMatchedAny = {};% make a cell array of all posRates together
+for i = 1:size(posRatesCellAnyPC, 2)
+    PCMatchedAny{i} = [];
+    for ii = 1:size(posRatesCellAnyPC, 1)
+        PCMatchedAny{i} = [PCMatchedAny{i}; posRatesCellAnyPC{ii, i}];
+    end
+end
+remapStrucSubsets.PCMatchedAny = [PCMatchedAny];
+
+%sort second column and match 1 and 3 accordingly: 
+[~, s1] = nanmax(PCMatchedAny{2}, [], 2);
+[~, s2] = sort(s1);
+for i = 1:length(PCMatchedAny)
+    PCMatchedAny{i} = PCMatchedAny{i}(s2, :);
+end
+figure;
+for i = 1:3
+    subplot(1, 3, i);
+    imagesc(PCMatchedAny{i});
+end
+
+%normalize each by max firing rate
+for i = 1:length(PCMatchedAny)
+    for ii = 1:size(PCMatchedAny{i}, 1)
+        PCMatchedAny{i}(ii, :) = PCMatchedAny{i}(ii, :)/nanmax(PCMatchedAny{i}(ii, :));
+    end
+    PCMatchedAny{i}(isnan(PCMatchedAny{i})) = 0;
+end
+
+figure;
+for i = 1:3
+    subplot(1, 3, i);
+    imagesc(PCMatchedAny{i});
+end
+
+%% now for all cells present in all sessions
+for i = 1:size(cellsInAll,1)
+    for j = 1:length(multSessSegStruc)
+        placeCellAllGoodSegInd(i,j) = find(multSessSegStruc(j).goodSeg == cellsInAll(i,j));
+        posRatesCell{i,j} = multSessSegStruc(j).outPC.posRates(placeCellAllGoodSegInd(i,j),:);
+    end
+end
+
+% correlate tuning for cells that are place cells in all sessions
+for i = 1:size(posRatesCell,1)
+    rates = [];
+    for j = 1:size(posRatesCell,2)
+        rates = [rates; posRatesCell{i,j}];
+    end
+    [remapStruc.cellsInAllCoef{i}, remapStruc.cellsInAllPval{i}] = corrcoef(rates');
+end
+
+A1A2=[]; A1B=[]; A2B=[];
+for i = 1:length(remapStruc.cellsInAllCoef)
+    A1A2 = [A1A2; remapStruc.cellsInAllCoef{i}(2)];
+    A1B = [A1B; remapStruc.cellsInAllCoef{i}(3)];
+    A2B = [A2B; remapStruc.cellsInAllCoef{i}(6)];
+end
+remapStruc.AllCorrCoeff121323 = [A1A2, A1B, A2B];
+%figure; pie([229 35 8 42]);
+
+
+%% find posRates for all cells in mapInd2/regMapOrigInd (ziv regist matr) 
+for i = 1:size(regMapOrigInd,1)
+    for j = 1:length(multSessSegStruc)
+        try
+            goodSegInd(i,j) = find(multSessSegStruc(j).goodSeg == regMapOrigInd(i,j));
+            posRatesCell{i,j} = multSessSegStruc(j).outPC.posRates(goodSegInd(i,j),:);
+        catch
+            posRatesCell{i,j} = [];
+        end
+    end
+end
+
+% correlate tuning for all cells in ziv matrix
+for i = 1:size(posRatesCell,1)
+    rates = [];
+    for j = 1:size(posRatesCell,2)
+        rates = [rates; posRatesCell{i,j}];
+    end
+    [remapStruc.allZivCorrCoef{i}, remapStruc.allZivCorrPval{i}] = corrcoef(rates');
+end
+
+
+%% now use this to compare correlations bet. 1-2, 2-3
+% 
+firstTwoPcell = remapStruc.allZivCorrPval(remapStruc.sessComb.firstTwoOnly);
+firstTwoCorrCell = remapStruc.allZivCorrCoef(remapStruc.sessComb.firstTwoOnly);
+for i = 1:length(firstTwoPcell)
+    firstTwoP(i)=firstTwoPcell{i}(2);
+    firstTwoCorr(i)=firstTwoCorrCell{i}(2);
+end
+
+lastTwoPcell = remapStruc.allZivCorrPval(remapStruc.sessComb.lastTwoOnly);
+lastTwoCorrCell = remapStruc.allZivCorrCoef(remapStruc.sessComb.lastTwoOnly);
+for i = 1:length(lastTwoPcell)
+    lastTwoP(i)=lastTwoPcell{i}(2);
+    lastTwoCorr(i)=lastTwoCorrCell{i}(2);
+end
+
+
+figure('Position', [50 50 800 400]); 
+subplot(1,2,1);
+f = firstTwoCorr(firstTwoP<0.05);
+l = lastTwoCorr(lastTwoP<0.05);
+bar([nanmean(f) nanmean(l)]); hold on;
+sem12 = nanstd(f)/sqrt(length(f));
+sem23 = nanstd(l)/sqrt(length(l));
+errorbar([nanmean(f) nanmean(l)],[sem12 sem23], '.');
+title('mean tuning correl, sig pval, sess 1-2 vs. 2-3');
+
+subplot(1,2,2);
+bar([nanmean(firstTwoCorr) nanmean(lastTwoCorr)]); hold on;
+sem12b = nanstd(firstTwoCorr)/sqrt(length(firstTwoCorr));
+sem23b = nanstd(lastTwoCorr)/sqrt(length(lastTwoCorr));
+errorbar([nanmean(firstTwoCorr) nanmean(lastTwoCorr)],[sem12b sem23b], '.');
+title('mean tuning correl, sess 1-2 vs. 2-3');
