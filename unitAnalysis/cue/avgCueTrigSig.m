319e13cfd264ad117e4373c412f0da6e9b0eaa96
claylacefield <claylacefield@gmail.com>
2019-08-28 18:01:36 -0400

    reconciling diffs

--- unitAnalysis/cue/avgCueTrigSig.m
+++ unitAnalysis/cue/avgCueTrigSig.m
@@ -97,9 +97,9 @@ filename = findLatestFilename('.xml');
 filename = filename(1:strfind(filename, '.xml')-1);
 
 %% Plotting
-<<<<<<< HEAD
+
 % if toPlot
-=======
+
 if toPlot
 figure;
 subplot(2,1,1);
@@ -145,7 +145,8 @@ yl = ylim; xl = xlim;
 line([30 30], yl);
 ylim(yl); xlim(xl);
 
->>>>>>> ef8bac3879c73b091bec3bfcd44d2579352419d9
+end
+
 % figure;
 % subplot(2,1,1);
 % try
